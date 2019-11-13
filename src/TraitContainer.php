faf7bbaf2ba9618bac70596d4d80cc44c70cab76
gbenga@emplug.com <N!55!Pen!e1>
2019-11-13 09:05:56 +0100

    PrimarySlotCommand 13112019

--- src/TraitContainer.php
+++ src/TraitContainer.php
@@ -4,24 +4,23 @@ declare(strict_types=1);
 namespace DV\MicroService;
 
 use Psr\Container\ContainerInterface;
-use Zend\ServiceManager\ServiceManager;
 
 trait TraitContainer
 {
     /**
-     * @var ContainerInterface | ServiceManager
+     * @var ContainerInterface
      */
     protected $container ;
 
-    public function setContainer($container)
+    public function setContainer(ContainerInterface $container) : void
     {
         $this->container = $container ;
     }
 
     /**
-     * @return ContainerInterface | ServiceManager
+     * @return ContainerInterface
      */
-    public function getContainer()
+    public function getContainer() : ContainerInterface
     {
         return $this->container ;
     }
